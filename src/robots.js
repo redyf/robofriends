ab73c0762d3c0042bfa5423354eb54cbf65e88d3
Redyf <mateusalvespereira7@gmail.com>
2023-08-20 22:28:27 -0300

    Changed the emails

--- src/robots.js
+++ src/robots.js
@@ -3,60 +3,60 @@ export const robots = [
     id: 1,
     name: 'Filipe Oliveira',
     username: 'uGamerCrazy',
-    email: 'Sincere@april.biz'
+    email: 'uGamerCrazy@april.biz'
   },
   {
     id: 2,
     name: 'Brururunu Perzentino',
     username: 'Batatão',
-    email: 'Shanna@melissa.tv'
+    email: 'Bururunu@business.com'
   },
   {
     id: 3,
     name: 'Ricardo Ramos',
     username: 'Destiny2player',
-    email: 'Nathan@yesenia.net'
+    email: 'Destiny2player@yesenia.net'
   },
   {
     id: 4,
     name: 'Geovani Italiano',
     username: 'Pizza',
-    email: 'Geovani.OConner@kory.org'
+    email: 'Luigipizza@kory.org'
   },
   {
     id: 5,
     name: 'João Lucas',
     username: '300reaisnovalorant',
-    email: 'Lucio_Hettinger@annie.ca'
+    email: '300reaisnovalorant@annie.com'
   },
   {
     id: 6,
     name: 'Daniel Radiant',
     username: 'valoranbestgame',
-    email: 'Karley_Dach@jasper.info'
+    email: 'Valoranbestgame@jasper.info'
   },
   {
     id: 7,
     name: 'José Bruno',
     username: 'Otaku123',
-    email: 'Anime.Hoeger@billy.biz'
+    email: 'PHPbestlanguage@fax.com'
   },
   {
     id: 8,
     name: 'Nathan cara de óculos',
     username: 'Thesunscaresme',
-    email: 'Nathan@rosamond.me'
+    email: 'Thesunscaresme@rosamond.me'
   },
   {
     id: 9,
     name: 'Gabriel Miranda',
     username: 'IuseArchBtw',
-    email: 'Chaim_McDermott@dana.io'
+    email: 'IuseArchBtw@dana.io'
   },
   {
     id: 10,
     name: 'Iruma Kun',
     username: 'Irumax',
-    email: 'Rey.Padberg@karina.biz'
+    email: 'BestAnime@lol.biz'
   }
 ];
